084af235b4a4efc0ade1ea18e83bef67cb8c7484
matrixben <ben354164737@qq.com>
2017-07-20 15:27:33 +0800

    add generator

--- hello_python.py
+++ hello_python.py
@@ -1,2 +1,15 @@
+import re
+
 text = 'python'
-print("Welcome to the {} world.".format(text))
+message = 'Welcome to the {} world.'.format(text)
+print(message)
+
+re_word = re.compile('\w+')
+
+
+def print_words(msg):
+    for match in re_word.finditer(msg):
+        yield match.group()
+
+for p in print_words(message):
+    print(p)
